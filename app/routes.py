a15970119ba3eb08abd0d88348d88b6aa1b804d2
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-08-22 03:11:16 -0500

    Write invalid data type test, make test pass with implementing code

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,9 @@ from flask import request
 
 @app.route('/api', methods=['POST'])
 def sole_api_endpoint():
-    message = request.json
-    print(message)
-    return message
+    if request.is_json:
+        message = request.json
+        # print(message)
+        return message
+    else:
+        return 'Error: request is not JSON'
--- tests.py
+++ tests.py
@@ -15,6 +15,11 @@ class AllTests(TestCase):
         response = self.client.post('/api', data=encoded_json, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, json.loads(encoded_json))
+
+    def test_post_request_without_json(self):
+        response = self.client.post('/api', data='Random text!')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), 'Error: request is not JSON')
     
     def test_invalid_request_method(self):
         response = self.client.get('/api')
