feec2a04be5f57d0f22efe4d2729c64c5e356c55
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-08-22 03:04:25 -0500

    Enhance tests

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,5 @@ from flask import request
 @app.route('/api', methods=['POST'])
 def sole_api_endpoint():
     message = request.json
+    print(message)
     return message
--- tests.py
+++ tests.py
@@ -3,10 +3,18 @@ from flask_testing import TestCase
 
 from app import app
 
+import json
+
 class AllTests(TestCase):
     
     def create_app(self):
         return app
+    
+    def test_expected_request(self):
+        encoded_json = json.dumps({'latitude':45.674, 'longitude':100.234})
+        response = self.client.post('/api', data=encoded_json, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, json.loads(encoded_json))
     
     def test_invalid_request_method(self):
         response = self.client.get('/api')
