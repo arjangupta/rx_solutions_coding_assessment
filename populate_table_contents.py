4af7ad93f4bae2276f51c4374c9bb7bc8fbc5cea
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-08-26 13:42:04 -0500

    Strip trailing whitespace from pharamcy names

--- populate_table_contents.py
+++ populate_table_contents.py
@@ -6,13 +6,15 @@ def csv_to_db():
     with open('pharmacies.csv', 'r') as csv_file:
         pharmacy_records = csv.reader(csv_file)
         first_line = True
-        display_csv_lines = False
+        display_csv_lines = True
         for row in pharmacy_records:
             if first_line:
                 first_line = False
                 continue
 
             # CSV format: pharmacy name, address, city, state, zip, latitude, longitude
+
+            row[0] = row[0].strip() #remove trailing whitespace from pharamcy names
             
             if display_csv_lines:
                 print("pharmacy_name=" + row[0] + \
