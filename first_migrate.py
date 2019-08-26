ec0ba3a3aac66a33fe2f3f2c849919e2d113de44
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-08-26 06:15:41 -0500

    Use csv.reader() for great csv support

--- first_migrate.py
+++ first_migrate.py
@@ -1,30 +1,43 @@
-import sys
+import csv
 from app import db
 from app.models import Pharmacy
 
 def csv_to_db():
-    f = open("pharmacies.csv", "r")
-    all_lines = f.readlines()
-    f.close()
-    all_lines = all_lines[1:len(all_lines)-1] #skip the first line
-    for line in all_lines:
-        #format: pharmacy name, address, city, state, zip, latitude, longitude
-        comma_separated_line = line.split(',')
-        print(pharmacy_name=comma_separated_line[0], \
-                address=comma_separated_line[1], \
-                city=comma_separated_line[2], \
-                state=comma_separated_line[3], \
-                zip=comma_separated_line[4], \
-                latitude=comma_separated_line[5], \
-                longitude=comma_separated_line[6]))
+    with open('pharmacies.csv', 'r') as csv_file:
+        pharmacy_records = csv.reader(csv_file)
+        for row in pharmacy_records:
+            print("pharmacy_name=" + row[0] + \
+                " address=" + row[1] + \
+                " city=" + row[2] + \
+                " state=" + row[3] + \
+                " zip=" + row[4] + \
+                " latitude=" + row[5] + \
+                " longitude=" + row[6])
 
-    #     new_pharmacy = Pharmacy(pharmacy_name=comma_separated_line[0], \
-    #                             address=comma_separated_line[1], \
-    #                             city=comma_separated_line[2], \
-    #                             state=comma_separated_line[3], \
-    #                             zip=comma_separated_line[4], \
-    #                             latitude=comma_separated_line[5], \
-    #                             longitude=comma_separated_line[6])
+
+    # f = open("pharmacies.csv", "r")
+    # all_lines = f.readlines()
+    # f.close()
+    # all_lines = all_lines[1:len(all_lines)-1] #skip the first line
+    # for line in all_lines:
+    #     #format: pharmacy name, address, city, state, zip, latitude, longitude
+    #     line = line.replace('\n"', '')
+    #     row = line.split(',')
+    #     print("pharmacy_name=" + row[0] + \
+    #             " address=" + row[1] + \
+    #             " city=" + row[2] + \
+    #             " state=" + row[3] + \
+    #             " zip=" + row[4] + \
+    #             " latitude=" + row[5] + \
+    #             " longitude=" + row[6])
+
+    #     new_pharmacy = Pharmacy(pharmacy_name=row[0], \
+    #                             address=row[1], \
+    #                             city=row[2], \
+    #                             state=row[3], \
+    #                             zip=row[4], \
+    #                             latitude=row[5], \
+    #                             longitude=row[6])
     #     db.session.add(new_pharmacy)
     # db.session.commit()
 
