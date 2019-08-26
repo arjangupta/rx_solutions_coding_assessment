d9584310bb4a4e46d1140525c6f9d4c3090a15e3
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-08-26 00:43:13 -0500

    Enhance model with indicating uniqueness and indexing of type attritbutes

--- app/models.py
+++ app/models.py
@@ -4,12 +4,12 @@ class Pharmacy(db.Model):
     #format: pharmacy name, address, city, state, zip, latitude, longitude
     id = db.Column(db.Integer, primary_key=True)
     pharmacy_name = db.Column(db.String(64))
-    address = db.Column(db.String(64))
+    address = db.Column(db.String(64), unique=True, index=True)
     city = db.Column(db.String(64))
     state = db.Column(db.String(64))
     zip = db.Column(db.String(64))
-    latitude = db.Column(db.String(64))
-    longitude = db.Column(db.String(64))
+    latitude = db.Column(db.String(64), index=True)
+    longitude = db.Column(db.String(64), index=True)
 
     def __repr__(self):
         return '<Pharmacy {}>'.format(self.pharmacy_name)
