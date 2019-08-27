deb4a84144404d3ab2fefb4d3ed2c38a9e7573f1
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-08-26 23:59:34 -0500

    Finished implementing required functionality

--- app/routes.py
+++ app/routes.py
@@ -10,14 +10,19 @@ pharmacies_by_latitude = Pharmacy.query.order_by(Pharmacy.latitude).all()
 def find_closest_pharmacy(user_latitude, user_longitude):
     user_coordinate = (user_latitude, user_longitude)
     closest_pharmacy = pharmacies_by_latitude[0]
-    minimum_displacement = geopy.distance.distance(user_coordinate, (closest_pharmacy.latitude, closest_pharmacy.longitude)).miles
+
+    minimum_displacement = geopy.distance.distance(user_coordinate, \
+            (closest_pharmacy.latitude, closest_pharmacy.longitude)).miles
+
     for p in pharmacies_by_latitude[1:]:
         pharmacy_coordinate = (p.latitude, p.longitude)
-        displacement = geopy.distance.distance(user_coordinate, pharmacy_coordinate).miles
+        displacement = geopy.distance.distance(user_coordinate, \
+                                               pharmacy_coordinate).miles
         if displacement < minimum_displacement:
             minimum_displacement = displacement
             closest_pharmacy = p
-    return closest_pharmacy
+    
+    return closest_pharmacy, minimum_displacement
 
 # This end-point expects a request with
 # data type JSON that has only two keys, 
@@ -28,9 +33,18 @@ def sole_api_endpoint():
     if request.is_json:
         json_dict = request.json
         if 'user_latitude' in json_dict and 'user_longitude' in json_dict:
-            closest_pharmacy = find_closest_pharmacy(json_dict.get('user_latitude'), json_dict.get('user_longitude'))
-            return 'Nearest pharmacy is unknown'
+            closest_pharmacy, min_dist = \
+                    find_closest_pharmacy(json_dict.get('user_latitude'), \
+                                          json_dict.get('user_longitude'))
+            full_address = closest_pharmacy.address + ', ' + \
+                           closest_pharmacy.city + ', ' + \
+                           closest_pharmacy.state + ' ' + \
+                           closest_pharmacy.zip
+            return dict(pharmacy_name=closest_pharmacy.pharmacy_name, \
+                                   address=full_address, \
+                                   distance=min_dist)
         else:
-            return 'Error: request does not contain user\'s latitude and longitude', 400
+            return 'Error: request does not contain user\'s latitude and ' \
+                    + 'longitude', 400
     else:
         return 'Error: request is not JSON', 400
--- tests.py
+++ tests.py
@@ -11,32 +11,39 @@ class AllTests(TestCase):
         return app
     
     def test_expected_request(self):
-        encoded_json = json.dumps({'user_latitude':45.674, 'user_longitude':100.234})
+        encoded_json = json.dumps({'user_latitude':39.185811, 'user_longitude':-95.463779})
         response = self.client.post('/api', data=encoded_json, content_type='application/json')
+
+        expected_response = dict(pharmacy_name="HUNTERS RIDGE PHARMACY", \
+                                 address="3405 NW HUNTERS RIDGE TER STE 200, TOPEKA, KS 66618", \
+                                 distance=13.912812880008875)
         
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), 'Nearest pharmacy is unknown')
+        self.assertEqual(response.json, expected_response)
 
     def test_no_given_latitude(self):
         encoded_json = json.dumps({'user_longitude':-6.55554})
         response = self.client.post('/api', data=encoded_json, content_type='application/json')
 
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.data.decode(), 'Error: request does not contain user\'s latitude and longitude')
+        self.assertEqual(response.data.decode(), 'Error: request does not contain user\'s ' \
+                                                 'latitude and longitude')
 
     def test_no_given_longitude(self):
         encoded_json = json.dumps({'user_latitude':-12312.55554})
         response = self.client.post('/api', data=encoded_json, content_type='application/json')
 
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.data.decode(), 'Error: request does not contain user\'s latitude and longitude')
+        self.assertEqual(response.data.decode(), 'Error: request does not contain user\'s ' \
+                                                 'latitude and longitude')
 
     def irrelevant_json(self):
         encoded_json = json.dumps({'foo':'bar'})
         response = self.client.post('/api', data=encoded_json, content_type='application/json')
 
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.data.decode(), 'Error: request does not contain user\'s latitude and longitude')
+        self.assertEqual(response.data.decode(), 'Error: request does not contain user\'s ' \
+                                                 'latitude and longitude')
 
     def test_invalid_data_type(self):
         response = self.client.post('/api', data='Random text!')
