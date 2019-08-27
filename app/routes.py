3c091af4096ce777a1b3b3090945e3a449adc285
Arjan Gupta <arjan.gupta@eyeverify.com>
2019-08-26 19:53:53 -0500

    Get list of pharmacies from db, display the user latitude and longitude

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,22 @@
 from app import app
+from app.models import Pharmacy
 from flask import request
+
+# Get list of pharmacies 
+pharmacies_by_latitude = Pharmacy.query.order_by(Pharmacy.latitude).all()
 
 # This end-point expects a request with
 # data type JSON that has only two keys, 
 # user_latitude and user_longitude.
 @app.route('/api', methods=['POST'])
 def sole_api_endpoint():
+    global pharmacies_by_latitude
     if request.is_json:
         json_dict = request.json
         if 'user_latitude' in json_dict and 'user_longitude' in json_dict:
+            user_latitude  = json_dict.get('user_latitude')
+            user_longitude = json_dict.get('user_longitude')
+            print(f'User latitude: {user_latitude}, and longitude: {user_longitude}')
             return 'Nearest pharmacy is unknown'
         else:
             return 'Error: request does not contain user\'s latitude and longitude', 400
